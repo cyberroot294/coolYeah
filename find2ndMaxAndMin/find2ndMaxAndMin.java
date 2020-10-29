e51c9fcb45b42005b12c6658cc1c694d3f6605eb
Nabiel Omar Syarif <nabilumarsyarif294@gmail.com>
2020-10-30 00:43:50 +0700

    changes find2ndMaxAndMin java file, membuat program flexibel dan bisa menerima input elemen array yang nilainya sama

--- find2ndMaxAndMin/find2ndMaxAndMin.java
+++ find2ndMaxAndMin/find2ndMaxAndMin.java
@@ -1,41 +1,70 @@
 package find2ndMaxAndMin;
-
 import java.util.Scanner;
 
 public class find2ndMaxAndMin {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int[] arr = new int[n];
+		int n = 0; 
+		int j = 0; 
+		int nilai[];
+		int counter = 0; // untuk menghitung banyak elemen yang sama di dalam array
+		Scanner input = new Scanner (System.in);
+		
+		System.out.print("Masukkan n banyaknya nilai : ");
+		n = input.nextInt();
+		
+		nilai = new int [n];
+		
+		for (int i = 0; i < n; i++) {
+			System.out.print("Masukkan nilai ke-" + (i + 1) + " : ");
+			nilai [i] = input.nextInt();
+		}
+		
+		secondMaxMin(n, j, nilai, counter);
+		input.close();
+	}
+
+    private static void secondMaxMin(int n, int j, int[] nilai, int counter) {
+        int wadah;
         for (int i = 0; i < n; i++) {
-            arr[i] = in.nextInt();
-        }
-        in.close();
-        secondMaxMin(n, arr);
+			for (j = 0; j < (n-1); j++) {
+				if (nilai[j] >= nilai [(j+1)]) {
 
-    }
+					// swapping
+					wadah = nilai[j];
+					nilai[j] = nilai [(j+1)];
+					nilai [(j+1)] = wadah;
+				} 
 
-    public static void secondMaxMin(int n, int[] arr) {
-        for (int i = 0; i < n; i++) {
-            // asumsi nilai yang minus
-            int min = arr[i];
-            int idMin = i;
+				if (nilai[j] == nilai [(j+1)]) {
+					nilai [j] = 0;
+				}
+			}
+		}
+		
+		for (int i : nilai) {
+			if (i == 0) {
+				counter++;
+			}
+		}
 
-            for (int j = i+1; j < n; j++) {
-                if (arr[j] < min) {
-                    min = arr[j];
-                    idMin = j;
-                }
-            }
+		System.out.println("Nilai Maks ke dua " + nilai[(j-1)]);
 
-            // swapping
-            int temp = arr[i];
-            arr[i] = min;
-            arr[idMin] = temp;
+		int k = 0;;
+		for (int i = 0; i < n; i++) {
+			for (k = n-1; k >= 1; k--) {
+				if (nilai[k] <= nilai [k-1]) {
 
-        }
+					// swapping
+					wadah = nilai[k];
+					nilai[k] = nilai [k-1];
+					nilai [k-1] = wadah;
+				} 
+				if (nilai[k] == nilai [k-1]) {
+					nilai [k] = 0;
+				}
+			}
+		}
 
-        System.out.println("Nilai terkecil ke 2 ialah " + arr[1]);
-        System.out.println("Nilai terbesar ke 2 ialah " + arr[n-2]);
+		System.out.println("Nilai Min ke dua " + nilai[k+1+counter]);
     }
 }
